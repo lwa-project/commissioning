0b336bd1aaddc5d9b12822ae4cf29e831a81d2d1
Jayce Dowell <jayce.dowell@gmail.com>
2013-01-16 16:42:50 +0000

    Converted splitMultiVis.py to handle multiple files at once.

--- TBN/Calibration/splitMultiVis.py
+++ TBN/Calibration/splitMultiVis.py
@@ -18,50 +18,52 @@ MIN_INTS_TO_KEEP = 20
 
 
 def main(args):
-	filename = args[0]
-	dataDict = numpy.load(filename)
+	for filename in args:
+		dataDict = numpy.load(filename)
 	
-	print "Working on file '%s'" % filename
+		print "Working on file '%s'" % filename
 
-	# Load in the data
-	refAnt = dataDict['ref'].item()
-	refX   = dataDict['refX'].item()
-	refY   = dataDict['refY'].item()
-	tInt = dataDict['tInt'].item()
+		# Load in the data
+		refAnt = dataDict['ref'].item()
+		refX   = dataDict['refX'].item()
+		refY   = dataDict['refY'].item()
+		tInt = dataDict['tInt'].item()
 	
-	times = dataDict['times']
-	phase = dataDict['simpleVis']
+		times = dataDict['times']
+		phase = dataDict['simpleVis']
 	
-	centralFreqs = dataDict['centralFreqs']
+		centralFreqs = dataDict['centralFreqs']
 
-	# Find the unique sets of (non-zero) frequencies and report
-	uFreq = numpy.unique(centralFreqs)
-	uFreq = uFreq[numpy.where(uFreq != 0)]
-	print "  Found %i unique frequencies from %.3f to %.3f MHz" % (len(uFreq), uFreq.min()/1e6, uFreq.max()/1e6)
+		# Find the unique sets of (non-zero) frequencies and report
+		uFreq = numpy.unique(centralFreqs)
+		uFreq = uFreq[numpy.where(uFreq != 0)]
+		print "  Found %i unique frequencies from %.3f to %.3f MHz" % (len(uFreq), uFreq.min()/1e6, uFreq.max()/1e6)
 	
-	# Report on the start time
-	beginDate = datetime.utcfromtimestamp(times[0])
-	print "  Start date/time of data: %s UTC" % beginDate
+		# Report on the start time
+		beginDate = datetime.utcfromtimestamp(times[0])
+		print "  Start date/time of data: %s UTC" % beginDate
 	
-	# Split
-	for i,f in enumerate(uFreq):
-		## Select what we need and trim off the last index to deal 
-		## with frequency changes
-		toKeep = numpy.where( centralFreqs == f )[0]
-		toKeep = toKeep[:-1]
+		# Split
+		for i,f in enumerate(uFreq):
+			## Select what we need and trim off the last index to deal 
+			## with frequency changes
+			toKeep = numpy.where( centralFreqs == f )[0]
+			toKeep = toKeep[:-1]
 		
-		## Sub-sets of `times` and `phase`
-		subTimes = times[toKeep]
-		subPhase = phase[toKeep,:]
+			## Sub-sets of `times` and `phase`
+			subTimes = times[toKeep]
+			subPhase = phase[toKeep,:]
 		
-		if len(toKeep) < MIN_INTS_TO_KEEP:
-			print "  -> Skipping %.3f MHz with only %i integrations (%.1f s)" % (f, len(toKeep), len(toKeep)*tInt)
-			continue
+			if len(toKeep) < MIN_INTS_TO_KEEP:
+				print "  -> Skipping %.3f MHz with only %i integrations (%.1f s)" % (f, len(toKeep), len(toKeep)*tInt)
+				continue
 		
-		## Save the split data to its own file
-		outname = filename.replace('.npz', '-%03i.npz' % (i+1,))
-		numpy.savez(outname, ref=refAnt, refX=refX, refY=refY, tInt=tInt, centralFreq=f, 
-					times=subTimes, simpleVis=subPhase)
+			## Save the split data to its own file
+			outname = os.path.splitext(filename)[1]
+			outname = "%s-%03i.npz" % (outname, i+1)
+			print "    Saving visibility data to '%s'" % outname
+			numpy.savez(outname, ref=refAnt, refX=refX, refY=refY, tInt=tInt, centralFreq=f, 
+						times=subTimes, simpleVis=subPhase)
 
 
 if __name__ == "__main__":
