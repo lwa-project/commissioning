140b89e3259663415b9b9f7edaff406b4868c158
Jayce Dowell <jayce.dowell@gmail.com>
2013-10-16 19:01:30 +0000

    Added an optional to get a MJD/MPM value from a date and a LST.

--- time2time.py
+++ time2time.py
@@ -14,10 +14,12 @@ $LastChangedDate$
 import sys
 import math
 import pytz
-import datetime
 import getopt
+import datetime
 
+from lsl.common.stations import lwa1
 from lsl.common.mcs import datetime2mjdmpm
+from lsl.astro import date as astroDate, get_date as astroGetDate
 
 
 def usage(exitCode=None):
@@ -29,6 +31,7 @@ Usage: time2time.py [OPTIONS] YYYY/MM/DD HH:MM:SS[.SSS]
 
 Options:
 -h, --help                  Display this help information
+-s, --sidereal              Input time is in LST, not local
 """
 
 	if exitCode is not None:
@@ -39,10 +42,11 @@ Options:
 
 def parseOptions(args):
 	config = {}
+	config['local'] = True
 
 	# Read in and process the command line flags
 	try:
-		opts, args = getopt.getopt(args, "h", ["help",])
+		opts, args = getopt.getopt(args, "hs", ["help", "sidereal"])
 	except getopt.GetoptError, err:
 		# Print help information and exit:
 		print str(err) # will print something like "option -a not recognized"
@@ -52,6 +56,8 @@ def parseOptions(args):
 	for opt, value in opts:
 		if opt in ('-h', '--help'):
 			usage(exitCode=0)
+		elif opt in ('-s', '--sidereal'):
+			config['local'] = False
 		else:
 			assert False
 	
@@ -62,9 +68,38 @@ def parseOptions(args):
 	return config
 
 
+def _getEquinoxEquation(jd):
+	"""
+	Compute the equation of the equinoxes (nutation in right ascension) in 
+	hours for the specified Julian Date.
+	
+	From:
+	http://aa.usno.navy.mil/faq/docs/GAST.php
+	"""
+	
+	# Get the number of days since January 1, 2000 @ 12:00 UT
+	D = jd - 2451545.0
+
+	# Compute the obliquity
+	epsilon = 23.4393 - 0.0000004*D
+	
+	# Compute the mean longitude of the Sun
+	L = 280.47 + 0.98565*D
+	
+	# Compute the longitude of the Moon's ascending node
+	Omega = 125.04 - 0.052954*D
+	
+	# The nutation in the longitude (hours)
+	deltaPsi = -0.000319*math.sin(Omega*math.pi/180) - 0.000024*math.sin(2*L*math.pi/180)
+
+	# Return the equation of the equinoxes
+	return deltaPsi * math.cos(epsilon*math.pi/180.0)
+
+
 def main(args):
 	config = parseOptions(args)
 	
+	obs = lwa1.getObserver()
 	MST = pytz.timezone('US/Mountain')
 	UTC = pytz.utc
 	
@@ -72,18 +107,72 @@ def main(args):
 		dt = datetime.datetime.utcnow()
 		dt = UTC.localize(dt)
 	else:
-		args[0] = args[0].replace('-', '/')
-		year, month, day = args[0].split('/', 2)
-		hour, minute, second = args[1].split(':', 2)
+		config['args'][0] = config['args'][0].replace('-', '/')
+		year, month, day = config['args'][0].split('/', 2)
+		hour, minute, second = config['args'][1].split(':', 2)
 		iSeconds = int(float(second))
 		mSeconds = int(round((float(second) - iSeconds)*1000000))
-
-		dt = MST.localize(datetime.datetime(int(year), int(month), int(day), int(hour), int(minute), iSeconds, mSeconds))
+		
+		if config['local']:
+			# Mountain time
+			dt = MST.localize(datetime.datetime(int(year), int(month), int(day), int(hour), int(minute), iSeconds, mSeconds))
+		else:
+			# LST
+			dt = astroDate(int(year), int(month), int(day), 0, 0, 0)
+			jd = dt.to_jd()
+			
+			## Get the LST in hours
+			LST = int(hour) + int(minute)/60.0 + (iSeconds + mSeconds/1e6)/3600.0
+			
+			## Get the Greenwich apparent ST for LST using the longitude of 
+			## the site.  The site longitude is stored as radians, so convert
+			## to hours first.
+			GAST = LST - obs.long*12/math.pi
+			
+			## Get the Greenwich mean ST by removing the equation of the 
+			## equinoxes (or some approximation thereof)
+			GMST = GAST - _getEquinoxEquation(jd)
+			
+			## Get the value of D0, days since January 1, 2000 @ 12:00 UT, 
+			## and T, the number of centuries since the year 2000.  The value
+			## of T isn't terribly important but it is nice to include
+			D0 = jd - 2451545.0
+			T = D0 / 36525.0
+			
+			## Solve for the UT hour for this LST and map onto 0 -> 24 hours
+			## From: http://aa.usno.navy.mil/faq/docs/GAST.php
+			H  = GMST - 6.697374558 - 0.06570982441908*D0 - 0.000026*T**2
+			H /= 1.002737909350795
+			while H < 0:
+				H += 24/1.002737909350795
+			while H > 24:
+				H -= 24/1.002737909350795
+				
+			## Get the full Julian Day that this corresponds to
+			jd += H/24.0
+			
+			## Convert the JD back to a time and extract the relevant 
+			## quantities needed to build a datetime instance
+			dt = astroGetDate(jd)
+			year = dt.years
+			month = dt.months
+			day = dt.days
+			hour = dt.hours
+			minute = dt.minutes
+			second = int(dt.seconds)
+			microsecond = int((dt.seconds - second)*1e6)
+			## Trim the microsecond down to the millisecond level
+			microsecond = int(int(microsecond/1000.0)*1000)
+			
+			## Localize as the appropriate time zone
+			dt = UTC.localize(datetime.datetime(year, month, day, hour, minute, second, microsecond))
 		dt = dt.astimezone(UTC)
-	
+		
+	obs.date = dt.astimezone(UTC).strftime("%Y/%m/%d %H:%M:%S.%f")
 	mjd, mpm = datetime2mjdmpm(dt)
 	
 	print "Localtime: %s" % dt.astimezone(MST).strftime("%B %d, %Y at %H:%M:%S %Z")
+	print "LST: %s" % obs.sidereal_time()
 	print "MJD: %i" % mjd
 	print "MPM: %i" % mpm
 	
