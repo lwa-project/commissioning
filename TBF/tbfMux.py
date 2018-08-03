65c310b9452b315dbbce3e0eec8b032e8a28a55e
Jayce Dowell <jayce.dowell@gmail.com>
2018-08-03 21:12:37 +0000

    Fixed another bug in the tbfMux.py RawTBFFrameBuffer's fill frame creator.

--- TBF/tbfMux.py
+++ TBF/tbfMux.py
@@ -181,7 +181,7 @@ class RawTBFFrameBuffer(buffer.FrameBuffer):
         # Get out the frame parameters and fix-up the header
         chan = frameParameters
         fillFrame[12] = (chan & 0xFF00) >> 8
-        filLFrame[14] = (chan & 0x00FF)
+        filLFrame[13] = (chan & 0x00FF)
         
         # Zero the data for the fill packet
         fillFrame[24:] = '\x00'*(12*256*2)
