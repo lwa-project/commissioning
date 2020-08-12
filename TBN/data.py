ce5cc154e56acd992d916f39e7c3959b3f3e7eea
jaycedowell <jayce.dowell@gmail.com>
2020-08-12 14:57:37 -0600

    Made data.py more robust.

--- TBN/data.py
+++ TBN/data.py
@@ -7,16 +7,20 @@ from __future__ import print_function
 import os
 import shutil
 
+MODULE_PATH = os.path.dirname(os.path.abspath(__file__))
+MODULE_NAME = os.path.join(MODULE_PATH, '_data.py')
+SOURCE_NAME = os.path.join(MODULE_PATH, '../DRX/HDF5/data.py')
+
 # Make sure we have the newest version of the file.  If not, copy it over
 copy = False
-if os.path.exists('_data.py'):
-    if os.path.getmtime('../DRX/HDF5/data.py') > os.path.getmtime('_data.py'):
+if os.path.exists(MODULE_NAME):
+    if os.path.getmtime(SOURCE_NAME) > os.path.getmtime(MODULE_NAME):
         copy = True
 else:
     copy = True
     
 if copy:
-    shutil.copy('../DRX/HDF5/data.py', '_data.py')
+    shutil.copy(SOURCE_NAME, MODULE_NAME)
     
 # Load in everything from the module
 from _data import *
