2e9fcf94bd24c22cdb90f1e1260d2bd92e718b4d
jaycedowell <jayce.dowell@gmail.com>
2021-06-24 09:50:59 -0600

    Get the data not the data set.

--- DRX/HDF5/stokes2linear.py
+++ DRX/HDF5/stokes2linear.py
@@ -152,7 +152,7 @@ def main(args):
                 ## XX
                 print("      Computing 'XX'")
                 tuningOut.create_dataset('XX', tuningIn['I'].shape, dtype=tuningIn['I'].dtype.descr[0][1])
-                tuningOut['XX'][:] = (tuningIn['I'][:] + tuningIn['Q'])/2.0
+                tuningOut['XX'][:] = (tuningIn['I'][:] + tuningIn['Q'][:])/2.0
                 
                 for key in tuningIn['I'].attrs:
                     tuningOut['XX'].attrs[key] = tuningIn['I'].attrs[key]
@@ -174,7 +174,7 @@ def main(args):
                 ## YY
                 print("      Computing 'YY'")
                 tuningOut.create_dataset('YY', tuningIn['I'].shape, dtype=tuningIn['I'].dtype.descr[0][1])
-                tuningOut['YY'][:] = (tuningIn['I'][:] - tuningIn['Q'])/2.0
+                tuningOut['YY'][:] = (tuningIn['I'][:] - tuningIn['Q'][:])/2.0
                 
                 for key in tuningIn['I'].attrs:
                     tuningOut['YY'].attrs[key] = tuningIn['I'].attrs[key]
