81186696aaf065b8ae38fad226fea32e283266a5
jaycedowell <jaycedowell@gmail.com>
2022-12-09 09:59:33 -0700

    Change default intepreter to python3.

--- TBN/Prototype/checkTimetags.py
+++ TBN/Prototype/checkTimetags.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 """
 Check the time tags in a TBN file from the prototype system at the
--- TBN/Prototype/correlateTBN.py
+++ TBN/Prototype/correlateTBN.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 """
 Example script that reads in TBN data and runs a cross-correlation on it.  
