4032d9e61795604206995d0eba39f3b53c986468
jaycedowell <jaycedowell@gmail.com>
2024-04-23 13:21:19 -0600

    Initial commit of the diffuse sky model calibration method.

---
+++ TBF/DiffuseCalibration/applySelfCalTBW2.py
@@ -0,0 +1,224 @@
+#!/usr/bin/env python3
+    
+import os
+import sys
+import aipy
+import copy
+import pytz
+import numpy
+import argparse
+from calendar import timegm
+from datetime import datetime
+
+from lsl import astro
+from lsl.common import stations
+from lsl.statistics.robust import *
+from lsl.correlator import uvutils
+from lsl.writer.fitsidi import NUMERIC_STOKES
+from lsl.misc import parser as aph
+
+from lsl.skymap import SkyMapLFSM, ProjectedSkyMap
+from lsl.imaging import utils, selfcal, overlay
+from lsl.sim import vis as simVis
+
+import matplotlib.pyplot as plt
+from matplotlib.ticker import NullFormatter
+
+
+MST = pytz.timezone('US/Mountain')
+UTC = pytz.UTC
+
+
+def main(args):
+    filename = args.filename
+    
+    idi = utils.CorrelatedData(filename)
+    aa = idi.get_antennaarray()
+    lo = idi.get_observer()
+    lo.date = idi.date_obs.strftime("%Y/%m/%d %H:%M:%S")
+    jd = lo.date + astro.DJD_OFFSET
+    lst = str(lo.sidereal_time())
+
+    nStand = len(idi.stands)
+    nchan = len(idi.freq)
+    freq = idi.freq
+    
+    print("Raw Stand Count: %i" % nStand)
+    print("Final Baseline Count: %i" % (nStand*(nStand-1)//2,))
+    print("Spectra Coverage: %.3f to %.3f MHz in %i channels (%.2f kHz/channel)" % (freq[0]/1e6, freq[-1]/1e6, nchan, (freq[-1] - freq[0])/1e3/nchan))
+    print("Polarization Products: %i starting with %i" % (len(idi.pols), idi.pols[0]))
+    print("JD: %.3f" % jd)
+    
+    # Pull out something reasonable
+    toWork = numpy.where((freq>=args.lower) & (freq<=args.upper))[0]
+    
+    print("Reading in FITS IDI data")
+    nSets = idi.total_baseline_count // (nStand*(nStand+1)//2)
+    for set in range(1, nSets+1):
+        print("Set #%i of %i" % (set, nSets))
+        fullDict = idi.get_data_set(set)
+        dataDict = fullDict.get_uv_range(max_uv=4.0)
+        dataDict.sort()
+        
+        # Gather up the polarizations and baselines
+        pols = dataDict.pols
+        bls = dataDict.baselines
+        print("The reduced list has %i baselines and %i channels" % (len(bls), len(toWork)))
+        
+        # Build a list of unique JDs for the data
+        jdList = [dataDict.jd]
+        
+        # Build the simulated visibilities
+        print("Building Model")
+        sm = SkyMapLFSM(freq_MHz=freq.mean()/1e6)
+        pm = ProjectedSkyMap(sm, aa.lat*180/numpy.pi, aa.lon*180/numpy.pi, jdList[0])
+        pm.compute_visible_power()
+        SOURCES = {}
+        for i in range(pm.visibleRa.size):
+            SOURCES[f"pnt{i+1}"] = aipy.amp.RadioFixedBody(pm.visibleRa[i]*numpy.pi/180,
+                                                           pm.visibleDec[i]*numpy.pi/180,
+                                                           jys=pm.visibleNormalizedPower[i],
+                                                           index=-2.3)
+        print(f"Using a source catalog with {len(SOURCES)} entries")
+        simDict = simVis.build_sim_data(aa, SOURCES, jd=[jdList[0],], pols=pols, baselines=bls)
+        
+        print("Running self cal.")
+        simDict.sort()
+        dataDict.sort()
+        fixedDataXX, delaysXX = selfcal.delay_only(aa, dataDict, simDict, toWork, 'XX', ref_ant=args.reference, max_iter=60, delay_cutoff=0.2)
+        fixedDataYY, delaysYY = selfcal.delay_only(aa, dataDict, simDict, toWork, 'YY', ref_ant=args.reference, max_iter=60, delay_cutoff=0.2)
+        fixedFullXX = simVis.scale_data(fullDict, delaysXX*0+1, delaysXX)
+        fixedFullYY = simVis.scale_data(fullDict, delaysYY*0+1, delaysYY)
+       
+        cXX = cYY = True 
+        if cXX and cYY:
+            print("    Saving results")
+            outname = os.path.split(filename)[1]
+            outname = os.path.splitext(outname)[0]
+            outname = "%s.sc" % outname
+            fh = open(outname, 'w')
+            fh.write("################################\n")
+            fh.write("#                              #\n")
+            fh.write("# Columns:                     #\n")
+            fh.write("# 1) Stand number              #\n")
+            fh.write("# 2) X pol. amplitude          #\n")
+            fh.write("# 3) X pol. delay (ns)         #\n")
+            fh.write("# 4) Y pol. amplitude          #\n")
+            fh.write("# 5) Y pol. delay (ns)         #\n")
+            fh.write("#                              #\n")
+            fh.write("################################\n")
+            for i in range(delaysXX.size):
+                fh.write("%3i  %.6g  %.6g  %.6g  %.6g\n" % (idi.stands[i], 1.0, delaysXX[i], 1.0, delaysYY[i]))
+            fh.close()
+
+        # Build up the images for each polarization
+        if args.plot:
+            print("    Gridding")
+            toWork = numpy.where((freq>=30e6) & (freq<=82e6))[0]
+            try:
+                imgXX = utils.build_gridded_image(fullDict, size=80, res=0.5, pol='XX', chan=toWork)
+            except:
+                imgXX = None
+                
+            try:
+                imgYY = utils.build_gridded_image(fullDict, size=80, res=0.5, pol='YY', chan=toWork)
+            except:
+                imgYY = None
+                
+            try:
+                simgXX = utils.build_gridded_image(simDict, size=80, res=0.5, pol='XX', chan=toWork)
+            except:
+                simgXX = None
+            try:
+                simgYY = utils.build_gridded_image(simDict, size=80, res=0.5, pol='YY', chan=toWork)
+            except:
+                simgYY = None
+                
+            try:
+                fimgXX = utils.build_gridded_image(fixedFullXX, size=80, res=0.5, pol='XX', chan=toWork)
+            except:
+                fimgXX = None
+            try:
+                fimgYY = utils.build_gridded_image(fixedFullYY, size=80, res=0.5, pol='YY', chan=toWork)
+            except:
+                fimgYY = None
+                
+            # Plots
+            print("    Plotting")
+            fig = plt.figure()
+            ax1 = fig.add_subplot(3, 2, 1)
+            ax2 = fig.add_subplot(3, 2, 2)
+            ax3 = fig.add_subplot(3, 2, 3)
+            ax4 = fig.add_subplot(3, 2, 4)
+            ax5 = fig.add_subplot(3, 2, 5)
+            ax6 = fig.add_subplot(3, 2, 6)
+            for ax, img, pol in zip([ax1, ax2, ax3, ax4, ax5, ax6], [imgXX, imgYY, simgXX, simgYY, fimgXX, fimgYY], ['XX', 'YY', 'simXX', 'simYY', 'scalXX', 'scalYY']):
+                # Skip missing images
+                if img is None:	
+                    ax.text(0.5, 0.5, 'Not found in file', color='black', size=12, horizontalalignment='center')
+                    
+                    ax.xaxis.set_major_formatter( NullFormatter() )
+                    ax.yaxis.set_major_formatter( NullFormatter() )
+                    
+                    ax.set_title("%s @ %s LST" % (pol, lst))
+                    continue
+                
+                # Display the image and label with the polarization/LST
+                out = img.image(center=(80,80))
+                print(pol, out.min(), out.max())
+                #if pol == 'scalXX':
+                    #out = numpy.rot90(out)
+                    #out = numpy.rot90(out)
+                cb = ax.imshow(out, extent=(1,-1,-1,1), origin='lower', 
+                        vmin=img.image().min(), vmax=img.image().max())
+                fig.colorbar(cb, ax=ax)
+                ax.set_title("%s @ %s LST" % (pol, lst))
+                
+                # Turn off tick marks
+                ax.xaxis.set_major_formatter( NullFormatter() )
+                ax.yaxis.set_major_formatter( NullFormatter() )
+                
+                # Compute the positions of major sources and label the images
+                compSrc = {}
+                ax.plot(0, 0, marker='+', markersize=10, markeredgecolor='w')
+                for name,src in simVis.SOURCES.items():
+                    src.compute(aa)
+                    top = src.get_crds(crdsys='top', ncrd=3)
+                    az, alt = aipy.coord.top2azalt(top)
+                    compSrc[name] = [az, alt]
+                    if alt <= 0:
+                        continue
+                    ax.plot(top[0], top[1], marker='x', markerfacecolor='None', markeredgecolor='w', 
+                            linewidth=10.0, markersize=10)
+                    ax.text(top[0], top[1], name, color='white', size=12)
+                    
+                # Add lines of constant RA and dec.
+                overlay.graticule_radec(ax, aa)
+                
+            plt.show()
+            
+    print("...Done")
+
+
+if __name__ == "__main__":
+    numpy.seterr(all='ignore')
+    
+    parser = argparse.ArgumentParser(
+        description="self-calibrate a TBW FITS IDI file",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+        )
+    parser.add_argument('filename', type=str, 
+                        help='filename to calibrate')
+    parser.add_argument('-r', '--reference', type=aph.positive_int, default=1,
+                        help='reference stand to use')
+    parser.add_argument('-l', '--lower', type=aph.positive_float, default=35.0,
+                        help='lowest frequency to consider in MHz')
+    parser.add_argument('-u', '--upper', type=aph.positive_float, default=85.0,
+                        help='highest frequency to consider in MHz')                  
+    parser.add_argument('-p', '--plot', action='store_true',
+                        help='plot the results at the end')
+    args = parser.parse_args()
+    args.lower *= 1e6
+    args.upper *= 1e6
+    main(args)
+    
---
+++ TBF/DiffuseCalibration/convertDelayToStretch.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python3
+
+# Python2 compatibility
+from __future__ import print_function, division
+try:
+    range = xrange
+except NameError:
+    pass
+    
+import os
+import sys
+import numpy
+import argparse
+
+from lsl.common.stations import parse_ssmif
+
+
+def main(args):
+    #
+    # Load in the data
+    #
+    site     = parse_ssmif(args.ssmif)
+    dataFile = [numpy.loadtxt(f) for f in args.filename]
+    
+    #
+    # Gather the station meta-data from its various sources
+    #
+    observer = site.get_observer()
+    antennas = site.antennas
+    
+    #
+    # Calculate the new stretch factors
+    #
+    output = [[None, None, None, None, None] for ant in antennas]
+    for i in range(dataFile[0].shape[0]):
+        ## Parse the line
+        stand, ampX, addDelayX, ampY, addDelayY = dataFile[0][i,:]
+        for df in dataFile[1:]:
+            _, aX, dX, aY, dY = df[i,:]
+            ampX += aX
+            addDelayX += dX
+            ampY += aY
+            addDelayY += dY
+        stand = int(stand)
+
+        ampX /= len(dataFile)
+        addDelayX /= len(dataFile)
+        ampY /= len(dataFile)
+        addDelayY /= len(dataFile)
+        
+        ## Pull out the cables
+        digX, digY = None, None
+        cableX, cableY = None, None
+        for ant in antennas:
+            if ant.stand.id == stand and ant.pol == 0:
+                digX = ant.digitizer
+                cableX = ant.cable
+            elif ant.stand.id == stand and ant.pol == 1:
+                digY = ant.digitizer
+                cableY = ant.cable
+                
+        ## Find the current stretch factor/delay
+        origX = cableX.stretch*1.0
+        origY = cableY.stretch*1.0
+        freq = numpy.linspace(35e6, 85e6, 101)
+        baseDelayX = cableX.delay(freq, ns=True)
+        baseDelayY = cableY.delay(freq, ns=True)
+        
+        bestX, bestY = 1e9, 1e9
+        stretchX, stretchY = 0.90, 0.90
+        for stretch in numpy.linspace(0.90, 1.10, 2001):
+            cableX.stretch = stretch
+            cableY.stretch = stretch
+            
+            newDelayX = cableX.delay(freq, ns=True)
+            newDelayY = cableY.delay(freq, ns=True)
+            
+            diffX = (newDelayX - baseDelayX).mean() - addDelayX
+            diffY = (newDelayY - baseDelayY).mean() - addDelayY
+            
+            if numpy.abs(diffX) < bestX:
+                bestX = numpy.abs(diffX)
+                stretchX = stretch
+            if numpy.abs(diffY) < bestY:
+                bestY = numpy.abs(diffY)
+                stretchY = stretch
+                
+        output[digX-1] = [digX, stretchX, addDelayX, bestX, 9.0]
+        output[digY-1] = [digY, stretchY, addDelayY, bestY, 9.0]
+        
+    #
+    # Report
+    #
+    if args.output is not None:
+        fh = open(args.output, 'w')
+    else:
+        fh = sys.stdout
+        
+    fh.write("##########################################\n")
+    fh.write("#                                        #\n")
+    fh.write("# Columns:                               #\n")
+    fh.write("# 1) Digitizer                           #\n")
+    fh.write("# 2) Cable length stretch factor         #\n")
+    fh.write("# 3) Additional delay over previous (ns) #\n")
+    fh.write("# 4) Mean delay error in new stretch     #\n")
+    fh.write("# 5) The number 9                        #\n")
+    fh.write("#                                        #\n")
+    fh.write("##########################################\n")
+    for entry in output:
+        try:
+            fh.write("%3i  %6.4f  %6.4f  %6.4f  %6.4f\n" % tuple(entry))
+        except TypeError:
+            pass
+        
+    if args.output is not None:
+        fh.close()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="given a file containing additional delays over the current SSMIF, convert the delays to new stretch factors",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+        )
+    parser.add_argument('ssmif', type=str,
+                        help='station SSMIF')
+    parser.add_argument('filename', type=str, nargs='+', 
+                        help='filename to convert')
+    parser.add_argument('-o', '--output', type=str,
+                        help='write output to the specified filename instead of the screen')
+    args = parser.parse_args()
+    main(args)
+    
