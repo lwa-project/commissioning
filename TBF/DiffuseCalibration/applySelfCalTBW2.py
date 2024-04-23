edba90d4fc3e5f148b1feb68eb9563cdd4e650f0
jaycedowell <jaycedowell@gmail.com>
2024-04-23 13:23:21 -0600

    It's TBF not TBW here.

--- TBF/DiffuseCalibration/applySelfCalTBW2.py
+++ TBF/DiffuseCalibration/applySelfCalTBW2.py
@@ -204,7 +204,7 @@ if __name__ == "__main__":
     numpy.seterr(all='ignore')
     
     parser = argparse.ArgumentParser(
-        description="self-calibrate a TBW FITS IDI file",
+        description="self-calibrate a TBF FITS IDI file",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
         )
     parser.add_argument('filename', type=str, 
