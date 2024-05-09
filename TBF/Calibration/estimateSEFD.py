c5d60494f7ba13ff570ffc2f22b16457a757b5a9
jaycedowell <jayce.dowell@gmail.com>
2024-05-09 10:40:36 -0600

    Update for TBF and running a basket weave.

--- TBF/Calibration/estimateSEFD.py
+++ TBF/Calibration/estimateSEFD.py
@@ -4,16 +4,14 @@ import os
 import sys
 import time
 import ephem
-import numpy
+import numpy as np
 import argparse
 from datetime import datetime, timedelta, tzinfo
 
 from lsl import astro
 from lsl.common.stations import parse_ssmif
-from lsl.reader.ldp import TBWFile
+from lsl.reader.ldp import TBFFile
 from lsl.misc import beamformer
-from lsl.common.dp import fS, SoftwareDP
-import lsl.correlator.fx as fxc
 
 from matplotlib import pyplot as plt
 
@@ -41,22 +39,8 @@ def main(args):
     # Get an observer reader for calculations
     obs = station.get_observer()
     
-    # Setup the beamformer gain and delay variables
-    course = numpy.zeros(520)
-    fine   = numpy.zeros(520)
-    gains  = numpy.zeros((260,4))
-    gains[:,0] = 1.0
-    gains[:,3] = 1.0
-    for ant in antennas:
-        if ant.combined_status != 33:
-            stand = (ant.digitizer - 1) / 2
-            gains[stand,:] = 0.0
-            
-    # Setup the beamformer itself
-    dp = SoftwareDP(mode='DRX', filter=7, central_freq=74e6)
-    
     # Find the target azimuth/elevation to use
-    idf = TBWFile(filenames[0])
+    idf = TBFFile(filenames[0])
     tStart = datetime.utcfromtimestamp(idf.get_info('start_time'))
     idf.close()
     
@@ -64,11 +48,11 @@ def main(args):
     tTransit = obs.next_transit(args.source)
     obs.date = tTransit
     args.source.compute(obs)
-    targetAz = args.source.az*180/numpy.pi
-    targetEl = args.source.alt*180/numpy.pi
+    targetAz = args.source.az*180/np.pi
+    targetEl = args.source.alt*180/np.pi
     
     # Preliminary report
-    print("Working on %i TBW files using SSMIF '%s'" % (len(filenames), os.path.basename(ssmif)))
+    print("Working on %i TBF files using SSMIF '%s'" % (len(filenames), os.path.basename(ssmif)))
     print("  Source: '%s'" % args.source.name)
     print("    Transit time: %s" % str(tTransit))
     print("    Transit azimuth: %.2f degrees" % targetAz)
@@ -79,19 +63,17 @@ def main(args):
     unx, lst, pwrX, pwrY = [], [], [], []
     for filename in filenames:
         ## Get the file reader
-        idf = TBWFile(filename)
+        idf = TBFFile(filename)
         
         ## Pull out some metadata and update the observer
         jd = astro.unix_to_utcjd(idf.get_info('start_time'))
         obs.date = ephem.Date(jd - astro.DJD_OFFSET)
         sample_rate = idf.get_info('sample_rate')
-        nInts = int(round( idf.get_info('nframe') / (30000.0 * len(antennas) / 2) ))
         transitOffset = (obs.date-tTransit)*86400.0
         
         ## Metadata report
         print("Filename: %s" % os.path.basename(filename))
         print("  Data type:  %s" % type(idf))
-        print("  Captures in file: %i (%.3f s)" % (nInts, nInts*30000*400/sample_rate))
         print("  Station: %s" % station.name)
         print("  Date observed: %s" % str(obs.date))
         print("  MJD: %.5f" % (jd-astro.MJD_OFFSET,))
@@ -100,54 +82,73 @@ def main(args):
         print(" ")
         
         ## Load in the data
-        readT, t, data = idf.read(time_in_samples=True)
+        readT, t, data = idf.read()
         
-        ## Build up a time array
-        t = t + numpy.arange(data.shape[1], dtype=numpy.int64)
+        ## Downselect to 74 +/- 8 MHz
+        freqs = idf.get_info('freq1')
+        valid_freq = np.where((freqs > 66e6) & (freqs < 82e6))[0]
+        freqs = freqs[valid_freq]
+        freqs.shape = (1,)+freqs.shape+(1,)
+        data = data[:,valid_freq,:]
         
-        ## Update the beamformer delays for the pointing center(s)
+        ## Come up with the pattern
+        pnts = []
+        ### Scale for whether or not it is a mini-station
+        pm_range = 8.0# if args.ministation else 4.0
+        ### First, declination
+        for offset in np.linspace(-pm_range, pm_range, 17):
+            pnts.append( (args.source._ra, ephem.degrees(args.source._dec+offset*np.pi/180)) )
+        ### Now, RA
+        for offset in np.linspace(-pm_range, pm_range, 17):
+            offset = offset / np.cos(src.dec)
+            pnts.append( (ephem.hours(args.source._ra+offset*np.pi/180), src._dec) )
+            
+        # Come up with the antenna gains
+        gains = np.zeros((len(antennas),1,1), dtype=np.float32)
+        for i,ant in enumerate(antennas):
+            if ant.combined_status != 33:
+                gains[(i//2)*2+0] = 0
+                gains[(i//2)*2+1] = 0
+        gaiX = gains*1.0
+        gaiX[1::2] = 0
+        gaiY = gains*1.0
+        gaiY[0::2] = 0
+        
         unx.append( idf.get_info('start_time') )
         lst.append( obs.sidereal_time() * 12/numpy.pi )
         pwrX.append( [] )
         pwrY.append( [] )
-        
-        for offset in (-1, 0, 1):
-            ### Compute
-            delays = beamformer.calc_delay(antennas, freq=74.0e6, azimuth=targetAz, elevation=targetEl+offset)
-            delays *= fS*16
-            delays = delays.max() - delays
-            ### Decompose into FIFO and FIR
-            course = (delays // 16)
-            fine   = (delays % 16)
+        for pnt in pnts:
+            bdy = ephem.FixedBody()
+            bdy._ra = pnt[0]
+            bdy._dec = pnt[1]
+            bdy._epoch = ephem.J2000
+            bdy.compute(obs)
             
-            ## Form the beams for both polarizations
-            beamX, beamY = dp.form_beam(antennas, t, data, course, fine, gains)
+            dlys = beamformer.calc_delays(antennas, freq=74e6, azimuth=bdy.az*180/np.pi, elevation=bdy.alt*180/np.pi)
+            dlys.shape = dlys.shape+(1,1)
+            phs = np.exp(-2j*np.pi*freqs*dlys)
             
-            ## Compute the integrated spectra
-            ### Convert to int16
-            beam = numpy.zeros((2, beamX.size), dtype=numpy.int16)
-            beam[0,:] = (numpy.round(beamX)).astype(data.dtype)
-            beam[1,:] = (numpy.round(beamY)).astype(data.dtype)
-            ### Move into the frequency domain
-            freq, spec = fxc.SpecMaster(beam, LFFT=8192, window=fxc.null_window, verbose=False, sample_rate=fS, clip_level=0)
+            beamX = (np.abs((gaiX*phs*data).sum(axis=0))**2).sum()
+            beamY = (np.abs((gaiY*phs*data).sum(axis=0))**2).sum()
             
             ## Save
-            pwrX[-1].append( spec[0,:] )
-            pwrY[-1].append( spec[1,:] )
+            pwrX[-1].append( beamX )
+            pwrY[-1].append( beamY )
             
         ## Done
         idf.close()
         
     # Convert to arrays
-    unx, lst = numpy.array(unx), numpy.array(lst)
-    pwrX, pwrY = numpy.array(pwrX), numpy.array(pwrY)
+    unx, lst = np.array(unx), np.array(lst)
+    pwrX, pwrY = np.array(pwrX), np.array(pwrY)
     
     # Save for later (needed for debugging)
     outname = "estimateSEFD-%s-%04i%02i%02i.npz" % (os.path.splitext(os.path.basename(ssmif))[0], tTransit.tuple()[0], tTransit.tuple()[1], tTransit.tuple()[2])
     print("Saving intermediate data to '%s'" % outname)
     print(" ")
-    numpy.savez(outname, source=args.source.name, freq=freq, 
-                unx=unx, lst=lst, pwrX=pwrX, pwrY=pwrY)
+    np.savez(outname, source=args.source.name, freq=freqs.mean(), 
+             unx=unx, lst=lst, pwrX=pwrX, pwrY=pwrY)
                 
     # Report
     print("%s" % (args.source.name,))
@@ -165,7 +166,7 @@ def main(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="given an SSMIF and a collection of TBW files, use the SoftwareDP to form beams at the transit point of a source and estimate the system equivalent flux density (SEFD) and pointing error",
+        description="given an SSMIF and a collection of TBF files, use phase-and-sum beamforming to make a basket weave pattern to estimate the system equivalent flux density (SEFD) and pointing error",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
         )
     parser.add_argument('ssmif', type=str,
@@ -178,4 +179,3 @@ if __name__ == "__main__":
                         help='show summary plots at the end')
     args = parser.parse_args()
     main(args)
-    
